45787d009a1badbc7f00d50631a2f8545c21e7d8
Neil Foster <ntfoster@gmail.com>
2020-10-12 21:01:05 +0100

    Initial commit

---
+++ create_pdfmarks.py
@@ -0,0 +1,31 @@
+import csv
+import argparse
+import sys
+
+description = "This program takes an input CSV file of bookmark names and page numbers and creates an output file of pdfmarks suitable for use with Ghostscript to create Bookmarks in a PDF file."
+parser = argparse.ArgumentParser(description=description)
+parser.add_argument("--input", "-i", help="input filename")
+args = parser.parse_args()
+
+def create_pdfmarks(input_filename):
+    # input_filename = 'se.csv'
+    output_filename = input_filename+'.pdfmarks'
+
+    with open(input_filename, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        output_file = open(output_filename,'w')
+        for row in reader:
+            title = row[0]
+            page = row[1]
+            output_file.write(f'[/Title ({title}) /Page {page} /OUT pdfmark\n')
+        output_file.close
+    csvfile.close
+
+if args.input:
+    create_pdfmarks(args.input)
+else:
+    print("You must specify an input filename with --input or -i")
+
+
+
+
