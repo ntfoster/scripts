6fe66034b4bb1c76a21890284d4d0de8444c43da
Neil Foster <ntfoster@gmail.com>
2020-10-25 17:14:15 +0000

    add option to keep originals when adding bookmarks

--- add_bookmarks.py
+++ add_bookmarks.py
@@ -12,6 +12,7 @@ parser = argparse.ArgumentParser(description=description)
 parser.add_argument("input", help="Original PDF")
 parser.add_argument("marks", help="File containing list of pdftk info")
 parser.add_argument("--output", "-o", help="Output file")
+parser.add_argument("--keep", action="store_true", help="keep copies of original files with '_original' suffix.", default=False)
 
 args = parser.parse_args()
 
@@ -34,5 +35,8 @@ else:
     output = os.path.splitext(input)[0]+'_bookmarked.pdf'
 
 add_bookmarks(input, pdfmarks, output)
-os.rename(input,os.path.splitext(input)[0]+'_original.pdf')
-os.rename(output,input)
+if args.keep:
+    os.replace(input,os.path.splitext(input)[0]+'_original.pdf')
+else:
+    os.remove(input)
+os.replace(output,input)
