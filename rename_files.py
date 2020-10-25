632b68ecd490eafb44161e0331f2dcedc3734eed
Neil Foster <ntfoster@gmail.com>
2020-10-25 19:01:16 +0000

    improve file handing

--- rename_files.py
+++ rename_files.py
@@ -1,6 +1,9 @@
 import os
 import re
 import argparse
+import shutil
+
+# An example input filename is "T9A-FB_LAB_UD_EN_v1_full_high.pdf"
 
 parser = argparse.ArgumentParser()
 parser.add_argument("files", type=argparse.FileType('r'), nargs='+', help="file to be renamed")
@@ -12,30 +15,30 @@ if args.version:
 else:
     version = "2020"
 
-files = args.file
-
-target_filename = "t9a-fb_fab_print_wotdg_2-2_en.pdf"
+files = args.files
 
 t9a_pattern = r't9a-fb_lab_(\w+)_(\w+)_v1_(\w+)_(\w+)\.pdf'
 t9a_re = re.compile(t9a_pattern)
 
 for f in files:
-    filename = os.path.basename(f)#.replace("_v1","")
+    with f:
+        filename = os.path.basename(f.name)
     result = t9a_re.match(filename.lower())
     if result:
-        # print (f)
         army = result.group(1)
         lang = result.group(2)
         format = result.group(3)
-        if format == "norules":
-            format = "background"
         quality = result.group(4)
         if quality == "high":
             quality = "print"
         if quality == "low":
             quality = "online"
-        # print(f"{result.group(1)} {result.group(2)} {result.group(3)} {result.group(4)}")
-        new_filename = f"t9a-fb_lab_{quality}_{army}_{format}_{version}_{lang}.pdf"
-        new_filename = new_filename.replace("_wdg_", "_wotdg_")
-        print(f'{f} => {new_filename}')
-        # os.rename(f,new_filename)
+        if format == "norules":
+            format = "background"
+            new_filename = f"t9a-fb_lab_{quality}_{army}_{format}_{lang}.pdf" # no version string needed for background book
+        else:
+            new_filename = f"t9a-fb_lab_{quality}_{army}_{format}_{version}_{lang}.pdf"
+        new_filename = os.path.dirname(f.name) + '\\' + new_filename.replace("_wdg_", "_wotdg_") # TODO: split off into separate function with dictionary of replacements
+    print(f'{f.name} => {new_filename}')
+    shutil.copy(f.name,new_filename)
+    os.remove(f.name)
